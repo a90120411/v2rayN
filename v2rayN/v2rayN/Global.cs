867d25dd37d92dd91e28ebc42f6205e92dca8e00
a90120411 <light901204@gmail.com>
2020-10-21 11:32:46 +0800

    与2dust的master同步代码，升级到v3.26版本

--- v2rayN/v2rayN/Global.cs
+++ v2rayN/v2rayN/Global.cs
@@ -14,7 +14,7 @@ namespace v2rayN
         /// <summary>
         /// SpeedTestUrl
         /// </summary>
-        public const string SpeedTestUrl = @"http://speedtest-sgp1.digitalocean.com/10mb.test";
+        public const string SpeedTestUrl = @"http://cachefly.cachefly.net/10mb.test";
         public const string SpeedPingTestUrl = @"https://www.google.com/generate_204";
         public const string AvailabilityTestUrl = @"https://www.google.com/generate_204";
 
@@ -103,6 +103,7 @@ namespace v2rayN
         /// 
         /// </summary>
         public const string StreamSecurity = "tls";
+        public const string StreamSecurityX = "xtls";
 
         public const string InboundSocks = "socks";
         public const string InboundHttp = "http";
@@ -116,13 +117,25 @@ namespace v2rayN
         /// </summary>
         public const string vmessProtocol = "vmess://";
         /// <summary>
+        /// vmess
+        /// </summary>
+        public const string vmessProtocolLite = "vmess";
+        /// <summary>
         /// shadowsocks
         /// </summary>
         public const string ssProtocol = "ss://";
         /// <summary>
+        /// shadowsocks
+        /// </summary>
+        public const string ssProtocolLite = "shadowsocks";
+        /// <summary>
         /// socks
         /// </summary>
         public const string socksProtocol = "socks://";
+        /// <summary>
+        /// socks
+        /// </summary>
+        public const string socksProtocolLite = "socks";
         /// <summary>
         /// http
         /// </summary>
@@ -131,6 +144,22 @@ namespace v2rayN
         /// https
         /// </summary>
         public const string httpsProtocol = "https://";
+        /// <summary>
+        /// vless
+        /// </summary>
+        public const string vlessProtocol = "vless://";
+        /// <summary>
+        /// vless
+        /// </summary>
+        public const string vlessProtocolLite = "vless";
+        /// <summary>
+        /// trojan
+        /// </summary>
+        public const string trojanProtocol = "trojan://";
+        /// <summary>
+        /// trojan
+        /// </summary>
+        public const string trojanProtocolLite = "trojan";
 
         /// <summary>
         /// pac
